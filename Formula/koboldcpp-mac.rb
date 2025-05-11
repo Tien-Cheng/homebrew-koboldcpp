1396768884725280a8f189fbf19b74faf02c2909
Tien-Cheng <tiencheng@protonmail.com>
2025-05-11 13:29:22 +0800

    fix: move url and sha256 block to top level to fix gh action runner

--- Formula/koboldcpp-mac.rb
+++ Formula/koboldcpp-mac.rb
@@ -4,11 +4,11 @@ class KoboldcppMac < Formula
   version "1.91"
   license "AGPL-3.0-only"
 
+  url "https://github.com/LostRuins/koboldcpp/releases/download/v#{version}/koboldcpp-mac-arm64"
+  sha256 "e0954ad0ce74f2b55a6420e6e7e0d6557cabdaa3a50a560b6ef64941a70a085d"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/LostRuins/koboldcpp/releases/download/v#{version}/koboldcpp-mac-arm64"
-      sha256 "e0954ad0ce74f2b55a6420e6e7e0d6557cabdaa3a50a560b6ef64941a70a085d"
       def install
         bin.install "koboldcpp-mac-arm64" => "koboldcpp"
       end
