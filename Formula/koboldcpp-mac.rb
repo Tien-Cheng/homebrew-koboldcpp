5fe51076ee0a01a933c2569ff74d4afbd51dc28a
Oh Tien Cheng <60145337+Tien-Cheng@users.noreply.github.com>
2025-10-12 08:11:58 +0000

    koboldcpp-mac 1.100

--- Formula/koboldcpp-mac.rb
+++ Formula/koboldcpp-mac.rb
@@ -1,11 +1,11 @@
 class KoboldcppMac < Formula
   desc "KoboldCpp (macOS ARM64): AI text-generation for GGML & GGUF models"
   homepage "https://github.com/LostRuins/koboldcpp"
-  version "1.98.1"
+  version "1.100"
   license "AGPL-3.0-only"
 
-  url "https://github.com/LostRuins/koboldcpp/releases/download/v1.98.1/koboldcpp-mac-arm64"
-  sha256 "57f296d59b8fce6e9588579469dc5ba40107f7edfe6ecf33bcb9d5b591f0d2cc"
+  url "https://github.com/LostRuins/koboldcpp/releases/download/v1.100/koboldcpp-mac-arm64"
+  sha256 "4f406f896d80c9e0b5357bb0e62e7b7cd882ecab96d3ba3ee2724c375afd8f02"
 
   on_macos do
     if Hardware::CPU.arm?
