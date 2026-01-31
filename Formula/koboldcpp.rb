d0b4b6241f963b0c12319835906a510b2f2e9526
Oh Tien Cheng <60145337+Tien-Cheng@users.noreply.github.com>
2026-01-31 08:16:01 +0000

    koboldcpp 1.107

--- Formula/koboldcpp.rb
+++ Formula/koboldcpp.rb
@@ -1,12 +1,12 @@
 class Koboldcpp < Formula
   desc "KoboldCpp: AI text-generation for GGML & GGUF models (CUDA 12.4, modern PCs)"
   homepage "https://github.com/LostRuins/koboldcpp"
-  version "1.102.3"
+  version "1.107"
   license "AGPL-3.0-only"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/LostRuins/koboldcpp/releases/download/v1.102.3/koboldcpp-linux-x64"
-    sha256 "442eaeca0d7324335bf214718f6f95012a79a0b4b9d55aa4434422e5ae2dd25c"
+    url "https://github.com/LostRuins/koboldcpp/releases/download/v1.107/koboldcpp-linux-x64"
+    sha256 "43dc6294b4d10c9a1c6ab54a5d9b301173cffae50d4b82dd0da1bd59a3cb3f9e"
   else
     odie "This formula is for Linux x86_64 only."
   end
